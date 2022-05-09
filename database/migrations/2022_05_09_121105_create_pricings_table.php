209309d2338fb2dc70d0e32886956ffc37fd1b68
rutvik <mohiterutvik6@gmail.com>
2022-05-09 19:07:15 +0530

    m

--- database/migrations/2022_05_09_121105_create_pricings_table.php
+++ database/migrations/2022_05_09_121105_create_pricings_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('price');
+            $table->string('technology');
             $table->timestamps();
         });
     }
